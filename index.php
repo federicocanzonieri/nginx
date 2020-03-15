b053803526219a1b82d4c1151389670155eee891
federicocanzonieri <50267192+federicocanzonieri@users.noreply.github.com>
2020-03-15 23:34:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php 
   echo "prova";
+
+  $dbServerName = "10.128.0.243";
+  $dbUsername = "root";
+  $dbPassword = "root";
+  $dbName = "mysql";
+
+  // create connection
+  $conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
+
+  // check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+
 ?>
