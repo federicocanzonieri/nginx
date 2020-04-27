44c23b99155e7dca20281014514d4b1a2f7396fd
federicocanzonieri <50267192+federicocanzonieri@users.noreply.github.com>
2020-04-27 19:21:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -39,9 +39,20 @@ if ($connection->connect_errno) {
     } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
+    $sql = "SELECT * FROM  Movie ";
+    $result = mysqli_query($connection, $sql); // First parameter is just return of "mysqli_connect()" function
+    echo "<br>";
+    echo "<table border='1'>";
+    while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
+        echo "<tr>";
+        foreach ($row as $field => $value) { // I you want you can right this line like this: foreach($row as $value) {
+            echo "<td>" . $value . "</td>"; // I just did not use "htmlspecialchars()" function. 
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
     
-}
+    }
 $connection->close();
 
 //mysql -u root -p
