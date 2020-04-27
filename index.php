0a529255f482783c8b51492ef913d1916bab881e
federicocanzonieri <50267192+federicocanzonieri@users.noreply.github.com>
2020-04-27 18:44:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,20 @@ if ($connection->connect_errno) {
     exit();
 } else {
     printf("Connected to the database");
+    $sql = "CREATE TABLE Movie (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    trama VARCHAR(255) NOT NULL,
+    anno INT(6),
+    durata VARCHAR(10)
+    )";
+    if ($connection->query($sql) === TRUE) {
+        echo "Table MyGuests created successfully";
+    } 
+    else {
+        echo "Error creating table: " . $conn->error;
+    }
+    
 }
 $connection->close();
 
