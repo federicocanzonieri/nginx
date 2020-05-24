1a3e2fbf416db995acfa2b9c9cf84787b0801713
federicocanzonieri <50267192+federicocanzonieri@users.noreply.github.com>
2020-05-24 19:09:25 +0200

    Update function.php

--- function.php
+++ function.php
@@ -12,7 +12,7 @@ $connection;
 function writeMsg() {
     echo "Hello world!<br>";
     //  MYSQL_SERVICE_HOST
-    echo "host: ".getenv("JENKINS_JNLP_PORT_50000_TCP_PORT")."<br>";
+    echo "host: ".getenv("MYSQL_SERVICE_HOST")."<br>";
     echo "port: ".getenv("MYSQL_SERVICE_PORT")."<br>";
     echo "user: ".getenv("databaseuser")."<br>";
     echo "password: ".getenv("databasepassword")."<br>";
